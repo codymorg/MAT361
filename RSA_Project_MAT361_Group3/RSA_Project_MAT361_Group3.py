15fec2ded147fcbe3eb1c66d17e551b30e0a5124
Cody Morgan <cody.morgan@digipen.edu>
2020-11-16 16:01:40 -0800

    encryption is ready for testing

--- RSA_Project_MAT361_Group3/RSA_Project_MAT361_Group3.py
+++ RSA_Project_MAT361_Group3/RSA_Project_MAT361_Group3.py
@@ -12,13 +12,56 @@ import os #displaying folder contents
 # This will never change for this project 
 n=777171741032261716925235958354065598504223
 e=131
-b=128
+blocksize=128//8
 d=231371739696627534045915232632172695126683
+
 
 # Global information
 IS_ENCRYPTING = ""
 FILEPATH = os.path.dirname(os.path.realpath(__file__))
 IN_DATA = ""
+BLOCKS = []
+OUT_DATA = ""
+
+# Functions
+def Modexp(a,x,m):
+    a = a % m
+    counter = 1
+    while x > 0:
+        if x % 2 == 1:
+            counter *= a
+            counter %= m
+        a **= 2
+        a %= m
+        x //= 2
+    return counter
+
+def Encode(message):
+    block = 0
+    encoded = []
+    for i in range(0,len(message)):
+        block = block * 100 + message[i]
+
+        if (i+1) % blocksize == 0:
+            encoded.append(block)
+            block = 0
+    # there may be an incomplete block
+    if block > 0:
+        encoded.append(block)
+
+    return encoded 
+
+def Encrypt(message):
+    encoded = Encode(message)
+    out = ""
+    for i in range(0,len(encoded)):
+        out+=str(Modexp(encoded[i],e,n))
+    return out
+
+def Decrypt(encrypted):
+    return Modexp(int(encrypted),d,n)
+
+### Run Program ###
 
 # User input to select encryption or decryption
 while (len(IS_ENCRYPTING) == 0 or (IS_ENCRYPTING != "E" and IS_ENCRYPTING != "D")):
@@ -55,12 +98,13 @@ while (len(FILEPATH) > 0):
                 break # we're done if a file is chosen
         except:
             print("Please enter valid file number\n")
-IN_DATA = open(FILEPATH, "r")
-
+IN_DATA = open(FILEPATH, "rb").read()
 
 # Encode (plain text -> blocks ready for encryption)
-# Encrypt (RSA encrypt)
-
+OUT_DATA = Encrypt(IN_DATA)
+encryptedFile = open('encrypted.txt', 'w')
+encryptedFile.write(OUT_DATA)
+encryptedFile.close()
 # Decrypt (decrpyt back into blocks)
 # Decode (blocks -> plaintext)
 
