7e8d057ac56ab8fa6ad9d4727d7df3165650f268
Cody Morgan <cody.morgan@digipen.edu>
2020-11-23 15:10:38 -0800

    merging with charles changes

--- RSA_Project_MAT361_Group3/RSA_Project_MAT361_Group3.py
+++ RSA_Project_MAT361_Group3/RSA_Project_MAT361_Group3.py
@@ -10,10 +10,10 @@
 import os #displaying folder contents
 
 # This will never change for this project 
-n=777171741032261716925235958354065598504223
+n=333303741167705894679341140877729876626667 
 e=131
-blocksize=128//8
-d=231371739696627534045915232632172695126683
+blocksize=14
+d=25443033676924114097557866558354696010331
 
 
 # Global information
@@ -40,33 +40,106 @@ def Encode(message):
     block = 0
     encoded = []
     for i in range(0,len(message)):
-        block = block * 100 + message[i]
-
+        block = block * 1000 + message[i]
         if (i+1) % blocksize == 0:
             encoded.append(block)
             block = 0
     # there may be an incomplete block
     if block > 0:
         encoded.append(block)
-
+    print("encoded",encoded[0])
     return encoded 
 
 def Encrypt(message):
     encoded = Encode(message)
+    while len(str(encoded[len(encoded)-1])) < (3*blocksize-2):
+        encoded[len(encoded)-1] = (encoded[len(encoded)-1]*1000) + 999
     out = ""
     for i in range(0,len(encoded)):
         out+=str(Modexp(encoded[i],e,n))
+        if i<len(encoded)-1:
+            out += " "
     return out
 
 def Decrypt(encrypted):
     return Modexp(int(encrypted),d,n)
+
+def Modinverse(a, m): 
+    m0 = m 
+    y = 0
+    x = 1
+  
+    if (m == 1): 
+        return 0
+  
+    while (a > 1): 
+        q = a // m 
+  
+        t = m 
+        m = a % m 
+        a = t 
+        t = y 
+  
+        y = x - q * y 
+        x = t 
+  
+    if (x < 0): 
+        x = x + m0 
+  
+    return x 
+
+def ElGamalSignature():
+  rk = Modexp(r, k, p_3)
+  rb = Modexp(r,b_3,p_3)
+  m = 2020
+  out = ""
+  out += "p = "
+  out += str(p_3)
+  out += "\n"
+  out += "r = "
+  out += str(r)
+  out += "\n"
+  out += "m = 2020\n"
+  out += "r^b = "
+  out += str(rb)
+  out += "\n"
+  out += "r^k = "
+  out += str(rk)
+  out += "\n"
+  out += "s = "
+
+  a = b_3 * rk
+  b = m - a
+
+  multinverse = Modinverse(k, p_3 - 1)
+
+  temp = (b * multinverse) % (p_3 - 1)
+  out += str(temp)
+  return out
+
+def RSASignature():
+  out = ""
+  m = 2020
+  out += "N = "
+  out += str(n)
+  out += "\n"
+  out += "e = "
+  out += str(e)
+  out += "\n"
+  out += "m = "
+  out += str(m)
+  out += "\n"
+  out += "m^d = "
+  out += str(Modexp(m,d,n))
+  return out
+
 
 ### Run Program ###
 
 # User input to select encryption or decryption
 while (len(IS_ENCRYPTING) == 0 or (IS_ENCRYPTING != "E" and IS_ENCRYPTING != "D")):
     print("[E]ncrpyt or [D]ecrypt?")
-    IS_ENCRYPTING = input()[0].upper()
+    IS_ENCRYPTING = "E"#input()[0].upper()
     print("")
 
 # User input to select file
